32c51daf89fef5f81cf88d1228443ee33b544407
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-17 01:20:16 +0700

    feat: added category listing, and searching

---
+++ lib/features/homepage/widgets/explore_list.dart
@@ -0,0 +1,94 @@
+import 'package:eocout_flutter/models/user_data.dart';
+import 'package:eocout_flutter/utils/business_type_util.dart';
+import 'package:eocout_flutter/utils/theme_data.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rxdart/rxdart.dart';
+
+class ExploreList extends StatelessWidget {
+  final BehaviorSubject<BusinessType> selectedBusiness;
+  const ExploreList({super.key, required this.selectedBusiness});
+
+  @override
+  Widget build(BuildContext context) {
+    UserData user = context.read<UserData>();
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hello, ${user.name}",
+                    style: textStyle.headlineSmall,
+                  ),
+                  const Text("Ayo lihat EO terbaru yang kami sediakan!")
+                ],
+              ),
+            ),
+            const Expanded(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: CircleAvatar(
+                  child: Icon(Icons.person),
+                ),
+              ),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        ListView.separated(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  selectedBusiness.add(BusinessType.values[index]);
+                },
+                child: Card(
+                  child: Container(
+                      height: 200,
+                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.black.withOpacity(0.3),
+                                  BlendMode.darken),
+                              image: AssetImage(BusinessTypeUtil.imageOf(
+                                  BusinessType.values[index])),
+                              fit: BoxFit.cover)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Lihat Semua",
+                            style: textStyle.bodyLarge!
+                                .copyWith(color: colorScheme.onPrimary),
+                          ),
+                          Text(
+                            BusinessTypeUtil.textOf(BusinessType.values[index]),
+                            style: textStyle.headlineLarge!
+                                .copyWith(color: colorScheme.onPrimary),
+                          ),
+                        ],
+                      )),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(
+                height: 10,
+              );
+            },
+            itemCount: BusinessType.values.length),
+      ],
+    );
+  }
+}
---
+++ lib/features/homepage/widgets/see_all_page.dart
@@ -0,0 +1,52 @@
+import 'package:eocout_flutter/components/my_searchbar.dart';
+import 'package:eocout_flutter/utils/business_type_util.dart';
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+
+class SeeAllPage extends StatefulWidget {
+  final BusinessType selectedBusiness;
+  const SeeAllPage({super.key, required this.selectedBusiness});
+
+  @override
+  State<SeeAllPage> createState() => _SeeAllPageState();
+}
+
+class _SeeAllPageState extends State<SeeAllPage> {
+  final _searchStream = BehaviorSubject<String>.seeded("");
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MySearchBar(
+            isRounded: true,
+            onChanged: (value) {
+              _searchStream.add(value);
+            },
+            label: "Cari EO atau Vendor yang kamu inginkan!"),
+        StreamBuilder(
+            stream: _searchStream,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                    child: Text("Tidak ada data yang ditemukan."));
+              }
+              return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return const ListTile(
+                      title: Text("Data"),
+                    );
+                  });
+            })
+      ],
+    );
+  }
+}
