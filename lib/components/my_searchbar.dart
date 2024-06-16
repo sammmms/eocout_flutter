e6503862928526d1ece150f365caa089342592f3
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-17 01:19:02 +0700

    feat: added rounded customization to search bar component

--- lib/components/my_searchbar.dart
+++ lib/components/my_searchbar.dart
@@ -5,13 +5,28 @@ import 'package:flutter_svg/flutter_svg.dart';
 class MySearchBar extends StatelessWidget {
   final void Function(String) onChanged;
   final String label;
-  const MySearchBar({super.key, required this.onChanged, required this.label});
+  final bool isRounded;
+  const MySearchBar(
+      {super.key,
+      required this.onChanged,
+      required this.label,
+      this.isRounded = false});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       onChanged: onChanged,
       decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderRadius:
+              isRounded ? BorderRadius.circular(30) : BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Colors.transparent),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius:
+              isRounded ? BorderRadius.circular(30) : BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Colors.transparent),
+        ),
         hintText: label,
         hintStyle: TextStyle(color: colorScheme.secondaryContainer),
         prefixIcon: Padding(
