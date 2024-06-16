5b1fcec6fe0b51828b682db03ec4caa10dfc4261
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-16 21:37:05 +0700

    feat: add a new reusable widget logo_with_title

---
+++ lib/features/authentication/widget/logo_with_title.dart
@@ -0,0 +1,36 @@
+import 'package:eocout_flutter/components/my_logo.dart';
+import 'package:eocout_flutter/utils/theme_data.dart';
+import 'package:flutter/material.dart';
+
+class LogoWithTitle extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  const LogoWithTitle({super.key, required this.title, this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        const MyLogo(size: 100),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          title,
+          style: textStyle.headlineLarge,
+          textAlign: TextAlign.center,
+        ),
+        if (subtitle != null)
+          Text(
+            subtitle!,
+            style: textStyle.titleLarge,
+            textAlign: TextAlign.center,
+          ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
