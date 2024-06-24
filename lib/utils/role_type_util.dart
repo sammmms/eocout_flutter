8dba3550940bb984be8c59633e1d83ceda54889d
sammmms <samuelonasis23@gmail.com>
2024-06-24 11:55:50 +0700

    fix: user role code to match backend api

--- lib/utils/role_type_util.dart
+++ lib/utils/role_type_util.dart
@@ -1,23 +1,34 @@
 import 'package:collection/collection.dart';
 
 enum UserRole {
-  basicUser,
+  user,
   eventOrganizer,
 }
 
 class UserRoleUtil {
   static const Map<UserRole, String> roleTypeMap = {
-    UserRole.basicUser: 'basicUser',
-    UserRole.eventOrganizer: 'eventOrganizer'
+    UserRole.user: 'User',
+    UserRole.eventOrganizer: 'Eo'
   };
 
   static String textOf(UserRole role) {
-    return roleTypeMap[role] ?? 'basicUser';
+    return roleTypeMap[role] ?? 'User';
+  }
+
+  static String readableTextOf(UserRole role) {
+    switch (role) {
+      case UserRole.user:
+        return 'User';
+      case UserRole.eventOrganizer:
+        return 'Event Organizer';
+      default:
+        return 'User';
+    }
   }
 
   static UserRole valueOf(String role) {
     return roleTypeMap.keys
             .firstWhereOrNull((key) => roleTypeMap[key] == role) ??
-        UserRole.basicUser;
+        UserRole.user;
   }
 }
