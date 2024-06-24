3691458068d6862d95ee441ec270eb331869fe51
sammmms <samuelonasis23@gmail.com>
2024-06-24 13:55:01 +0700

    feat: pick image dialog

---
+++ lib/components/my_pick_image.dart
@@ -0,0 +1,46 @@
+import 'package:eocout_flutter/utils/theme_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MyPickImage extends StatelessWidget {
+  const MyPickImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListTile(
+            leading: SvgPicture.asset(
+              "assets/svg/camera_icon.svg",
+              width: 20,
+              color: colorScheme.onBackground,
+            ),
+            title: const Text("Ambil Foto"),
+            onTap: () {
+              Navigator.pop(context, ImageSource.camera);
+            }),
+        ListTile(
+            leading: SvgPicture.asset(
+              "assets/svg/gallery_icon.svg",
+              width: 20,
+              color: colorScheme.onBackground,
+            ),
+            title: const Text("Pilih dari Galeri"),
+            onTap: () {
+              Navigator.pop(context, ImageSource.gallery);
+            }),
+        ListTile(
+            leading: Icon(
+              Icons.close,
+              color: colorScheme.onBackground,
+            ),
+            title: const Text("Batal"),
+            onTap: () {
+              Navigator.pop(context);
+            })
+      ],
+    );
+  }
+}
