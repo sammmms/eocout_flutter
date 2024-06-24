dff54aa82f05e93d276e420228be00f2093c10d8
sammmms <samuelonasis23@gmail.com>
2024-06-24 12:02:31 +0700

    feat: dot env load in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
+import 'package:eocout_flutter/bloc/authentication/authentication_bloc.dart';
+import 'package:eocout_flutter/bloc/authentication/authentication_state.dart';
+import 'package:eocout_flutter/features/dashboard_page.dart';
 import 'package:eocout_flutter/features/welcome_page.dart';
 import 'package:eocout_flutter/utils/theme_data.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  if (dotenv.isInitialized) {
+    if (kDebugMode) {
+      print('The .env file has been loaded');
+      print('The BASE URL is ${dotenv.env['BASE_URL']}');
+    }
+  }
+  // await Store.clearStore();
   runApp(const MainApp());
 }
 
