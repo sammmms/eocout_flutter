8bd717fdb902478459a7767d1595f6d68ab2fef3
sammmms <samuelonasis23@gmail.com>
2024-07-13 10:12:29 +0700

    fix: add event page scrollable appbar

--- lib/features/add_event/add_event_page.dart
+++ lib/features/add_event/add_event_page.dart
@@ -53,214 +53,220 @@ class _AddEventPageState extends State<AddEventPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tambahkan Layanan Kamu'),
-        backgroundColor: colorScheme.surface,
-        scrolledUnderElevation: 0,
-      ),
-      body: Center(
-          child: SingleChildScrollView(
-        padding: const EdgeInsets.fromLTRB(20, 20, 20, 100),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Upload Foto",
-                style: textStyle.titleMedium,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                height: 180,
-                child: Row(
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+            child: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 100),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Tambahkan Layanan Kamu',
+                  style: textStyle.headlineMedium,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Upload Foto",
+                  style: textStyle.titleMedium,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 180,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            border: Border.all(color: colorScheme.outline),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: SvgPicture.asset(
+                              'assets/svg/camera_icon.svg',
+                              height: 30,
+                              color: colorScheme.onSurface,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: colorScheme.outline),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: colorScheme.outline),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const Divider(
+                  height: 50,
+                ),
+                Text(
+                  "Informasi Layanan",
+                  style: textStyle.titleMedium,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Nama EO"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Nama EO tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    newEvent.eventOrganizerName = value;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Nama Layanan"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Nama Layanan tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    newEvent.eventName = value;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Harga"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Harga tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    newEvent.eventPrice = int.parse(value);
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Deskripsi"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Deskripsi tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    newEvent.eventDescription = value;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Lokasi"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Lokasi tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    newEvent.eventLocation = value;
+                  },
+                ),
+                const Divider(
+                  height: 40,
+                ),
+                Text(
+                  "Pemasaran",
+                  style: textStyle.titleMedium,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
                   children: [
                     Expanded(
-                      flex: 3,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          border: Border.all(color: colorScheme.outline),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: SvgPicture.asset(
-                            'assets/svg/camera_icon.svg',
-                            height: 30,
-                            color: colorScheme.onSurface,
-                          ),
-                        ),
+                      child: Text(
+                        "Apakah layanan anda memiliki promosi pemasaran atau promosi acara?",
+                        style: textStyle.titleSmall,
                       ),
                     ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                border: Border.all(color: colorScheme.outline),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                border: Border.all(color: colorScheme.outline),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                          ),
-                        ],
+                    Checkbox(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
                       ),
-                    )
+                      visualDensity: VisualDensity.compact,
+                      activeColor: colorScheme.tertiary,
+                      value: _isChecked,
+                      onChanged: (_) {
+                        setState(() {
+                          _isChecked = !_isChecked;
+                        });
+                      },
+                    ),
                   ],
                 ),
-              ),
-              const Divider(
-                height: 50,
-              ),
-              Text(
-                "Informasi Layanan",
-                style: textStyle.titleMedium,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Nama EO"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Nama EO tidak boleh kosong";
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  newEvent.eventOrganizerName = value;
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Nama Layanan"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Nama Layanan tidak boleh kosong";
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  newEvent.eventName = value;
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Harga"),
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Harga tidak boleh kosong";
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  newEvent.eventPrice = int.parse(value);
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Deskripsi"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Deskripsi tidak boleh kosong";
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  newEvent.eventDescription = value;
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Lokasi"),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Lokasi tidak boleh kosong";
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  newEvent.eventLocation = value;
-                },
-              ),
-              const Divider(
-                height: 40,
-              ),
-              Text(
-                "Pemasaran",
-                style: textStyle.titleMedium,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      "Apakah layanan anda memiliki promosi pemasaran atau promosi acara?",
-                      style: textStyle.titleSmall,
-                    ),
-                  ),
-                  Checkbox(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    visualDensity: VisualDensity.compact,
-                    activeColor: colorScheme.tertiary,
-                    value: _isChecked,
-                    onChanged: (_) {
-                      setState(() {
-                        _isChecked = !_isChecked;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                height: 54,
-                width: double.infinity,
-                child: OutlinedButton(
-                  onPressed: () {},
-                  child: const Text("Daftarkan Layanan"),
+                const SizedBox(
+                  height: 30,
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 54,
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: const Text("Daftarkan Layanan"),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      )),
+        )),
+      ),
     );
   }
 }
