a253195b17f6bb16a3b8144d67748c1a1c58b7dc
Sam! <156488637+sammmms@users.noreply.github.com>
2024-07-15 00:32:16 +0700

    feat: business data

---
+++ lib/models/business_data.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+import 'package:eocout_flutter/models/user_data.dart';
+
+class BusinessData {
+  final String id;
+  final String companyName;
+  final String description;
+  final List<File> images;
+  final bool isAcceptPartyPromotionOrMarketing;
+  final String location;
+  final String name;
+  final int price;
+  final UserData profile;
+
+  BusinessData({
+    required this.id,
+    required this.companyName,
+    required this.description,
+    this.images = const [],
+    this.isAcceptPartyPromotionOrMarketing = false,
+    this.location = "",
+    this.name = "",
+    this.price = 0,
+    UserData? profile,
+  }) : profile = profile ?? UserData();
+
+  factory BusinessData.fromJson(Map<String, dynamic> json,
+      [List<File>? images]) {
+    return BusinessData(
+      id: json['id'],
+      companyName: json['company_name'],
+      description: json['description'],
+      images: images ?? [],
+      isAcceptPartyPromotionOrMarketing:
+          json['is_accept_party_promotion_or_marketing'] ?? false,
+      location: json['location'],
+      name: json['name'],
+      price: int.tryParse(json['price']) ?? 0,
+      profile: UserData.fromJson(json['profile']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'company_name': companyName,
+      'price': price,
+      'description': description,
+      'images': images,
+      'is_accept_party_promotion_or_marketing':
+          isAcceptPartyPromotionOrMarketing,
+      'location': location,
+      'name': name,
+    };
+  }
+}
+
+class EditableBusinessData {
+  String companyName;
+  String description;
+  List<File> images;
+  bool isAcceptPartyPromotionOrMarketing;
+  String location;
+  String name;
+  int price;
+
+  EditableBusinessData({
+    this.companyName = "",
+    this.description = "",
+    List<File>? images,
+    this.isAcceptPartyPromotionOrMarketing = false,
+    this.location = "",
+    this.name = "",
+    this.price = 0,
+  }) : images = images ?? [];
+
+  Map<String, dynamic> toJson([List<String>? imageIds]) {
+    return {
+      'company_name': companyName,
+      'price': price,
+      'description': description,
+      'is_accept_party_promotion_or_marketing':
+          isAcceptPartyPromotionOrMarketing,
+      'location': location,
+      'name': name,
+      "image_ids": imageIds,
+    };
+  }
+}
