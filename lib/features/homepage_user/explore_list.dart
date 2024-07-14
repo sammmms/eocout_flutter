ad2820ab0fa8ae9b7a8bd88d44fec2cb997c629c
Sam! <156488637+sammmms@users.noreply.github.com>
2024-07-15 00:33:48 +0700

    feat: integrate explore list with category bloc

--- lib/features/homepage_user/explore_list.dart
+++ lib/features/homepage_user/explore_list.dart
@@ -1,21 +1,26 @@
 import 'package:eocout_flutter/bloc/authentication/authentication_bloc.dart';
+import 'package:eocout_flutter/bloc/category/category_bloc.dart';
+import 'package:eocout_flutter/bloc/category/category_state.dart';
 import 'package:eocout_flutter/components/my_homepage_appbar.dart';
 import 'package:eocout_flutter/utils/business_type_util.dart';
 import 'package:eocout_flutter/utils/theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class ExploreList extends StatelessWidget {
-  final BehaviorSubject<BusinessType?> selectedBusiness;
+  final BehaviorSubject<String?> selectedBusiness;
   const ExploreList({super.key, required this.selectedBusiness});
 
   @override
   Widget build(BuildContext context) {
     AuthBloc bloc = context.read<AuthBloc>();
+    CategoryBloc categoryBloc = context.read<CategoryBloc>();
     return RefreshIndicator(
       onRefresh: () async {
         await bloc.refreshProfile();
+        await categoryBloc.getCategories();
       },
       child: SingleChildScrollView(
         padding: const EdgeInsets.fromLTRB(20, 20, 20, 100),
@@ -25,53 +30,73 @@ class ExploreList extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            ListView.separated(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      selectedBusiness.add(BusinessType.values[index]);
-                    },
-                    child: Card(
-                      child: Container(
-                          height: 200,
-                          padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              image: DecorationImage(
-                                  colorFilter: ColorFilter.mode(
-                                      Colors.black.withOpacity(0.3),
-                                      BlendMode.darken),
-                                  image: AssetImage(BusinessTypeUtil.imageOf(
-                                      BusinessType.values[index])),
-                                  fit: BoxFit.cover)),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Lihat Semua",
-                                style: textStyle.bodyLarge!
-                                    .copyWith(color: colorScheme.onPrimary),
-                              ),
-                              Text(
-                                BusinessTypeUtil.textOf(
-                                    BusinessType.values[index]),
-                                style: textStyle.headlineLarge!
-                                    .copyWith(color: colorScheme.onPrimary),
-                              ),
-                            ],
-                          )),
+            StreamBuilder<CategoryState>(
+                stream: categoryBloc.controller,
+                builder: (context, snapshot) {
+                  bool isLoading =
+                      snapshot.data?.isLoading ?? false || !snapshot.hasData;
+
+                  // if(isLoading){
+                  //   return
+                  // }
+
+                  List<EOCategory> categories = snapshot.data?.categories ??
+                      List.generate(7, (_) => EOCategory.dummy());
+                  return Skeletonizer(
+                    enabled: isLoading,
+                    child: ListView.separated(
+                      itemCount: categories.length,
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        EOCategory category = categories[index];
+                        return GestureDetector(
+                          onTap: () {
+                            selectedBusiness.add(category.id);
+                          },
+                          child: Card(
+                            child: Container(
+                                height: 200,
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 20, 20, 10),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    image: DecorationImage(
+                                        colorFilter: ColorFilter.mode(
+                                            Colors.black.withOpacity(0.3),
+                                            BlendMode.darken),
+                                        image: AssetImage(
+                                            BusinessTypeUtil.imageOf(
+                                                category.businessType)),
+                                        fit: BoxFit.cover)),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Lihat Semua",
+                                      style: textStyle.bodyLarge!.copyWith(
+                                          color: colorScheme.onPrimary),
+                                    ),
+                                    Text(
+                                      BusinessTypeUtil.textOf(
+                                          category.businessType),
+                                      style: textStyle.headlineLarge!.copyWith(
+                                          color: colorScheme.onPrimary),
+                                    ),
+                                  ],
+                                )),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(
+                          height: 10,
+                        );
+                      },
                     ),
                   );
-                },
-                separatorBuilder: (context, index) {
-                  return const SizedBox(
-                    height: 10,
-                  );
-                },
-                itemCount: BusinessType.values.length),
+                }),
           ],
         ),
       ),
