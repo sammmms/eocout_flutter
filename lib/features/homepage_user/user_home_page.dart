0bf2adef10c42a52fe4245247aaa5a5c71941283
Sam! <156488637+sammmms@users.noreply.github.com>
2024-07-15 00:31:09 +0700

    feat: provide category bloc

--- lib/features/homepage_user/user_home_page.dart
+++ lib/features/homepage_user/user_home_page.dart
@@ -1,7 +1,7 @@
 import 'package:eocout_flutter/bloc/authentication/authentication_bloc.dart';
+import 'package:eocout_flutter/bloc/category/category_bloc.dart';
 import 'package:eocout_flutter/features/homepage_user/explore_list.dart';
 import 'package:eocout_flutter/features/homepage_user/see_all_page.dart';
-import 'package:eocout_flutter/utils/business_type_util.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rxdart/rxdart.dart';
@@ -14,13 +14,15 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  final _selectedBusiness = BehaviorSubject<BusinessType?>();
+  final _selectedBusiness = BehaviorSubject<String?>();
+  final CategoryBloc _categoryBloc = CategoryBloc();
   late AuthBloc bloc;
 
   @override
   void initState() {
     bloc = context.read<AuthBloc>();
     bloc.refreshProfile();
+    _categoryBloc.getCategories();
     super.initState();
   }
 
@@ -28,13 +30,17 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: StreamBuilder<BusinessType?>(
+      body: StreamBuilder<String?>(
           stream: _selectedBusiness,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               return SeeAllPage(selectedBusiness: _selectedBusiness);
             }
-            return ExploreList(selectedBusiness: _selectedBusiness);
+            return Provider<CategoryBloc>.value(
+                value: _categoryBloc,
+                child: ExploreList(
+                  selectedBusiness: _selectedBusiness,
+                ));
           }),
     ));
   }
