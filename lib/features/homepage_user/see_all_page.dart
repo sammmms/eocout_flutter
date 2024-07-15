2ab563c9b2521d7d0daf68bd3d233a806bf273e4
sammmms <samuelonasis23@gmail.com>
2024-07-15 13:30:25 +0700

    feat: loading in see all page, and refresh method

--- lib/features/homepage_user/see_all_page.dart
+++ lib/features/homepage_user/see_all_page.dart
@@ -1,13 +1,11 @@
-import 'dart:async';
-
 import 'package:eocout_flutter/bloc/service/service_bloc.dart';
 import 'package:eocout_flutter/bloc/service/service_state.dart';
 import 'package:eocout_flutter/components/my_searchbar.dart';
 import 'package:eocout_flutter/features/homepage_user/widget/business_card.dart';
 import 'package:eocout_flutter/models/business_data.dart';
-import 'package:eocout_flutter/utils/business_type_util.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class SeeAllPage extends StatefulWidget {
   final BehaviorSubject<String?> selectedBusiness;
@@ -20,10 +18,11 @@ class SeeAllPage extends StatefulWidget {
 class _SeeAllPageState extends State<SeeAllPage> {
   final _searchStream = BehaviorSubject<String>.seeded("");
   final _serviceBloc = ServiceBloc();
+  late String? categoryId;
 
   @override
   void initState() {
-    final String? categoryId = widget.selectedBusiness.valueOrNull;
+    categoryId = widget.selectedBusiness.valueOrNull;
     _serviceBloc.getServices(categoryId: categoryId!);
     _searchStream
         .debounceTime(const Duration(milliseconds: 500))
@@ -33,76 +32,81 @@ class _SeeAllPageState extends State<SeeAllPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: const NeverScrollableScrollPhysics(),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 20, 20, 100),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    widget.selectedBusiness.add(null);
-                  },
-                  child: const Icon(Icons.arrow_back),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Expanded(
-                  child: MySearchBar(
-                      isRounded: true,
-                      onChanged: (value) {
-                        _searchStream.add(value);
-                      },
-                      label: "Cari EO atau Vendor yang kamu inginkan!"),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            StreamBuilder<ServiceState>(
-                stream: _serviceBloc.stream,
-                builder: (context, snapshot) {
-                  bool isLoading =
-                      snapshot.data?.isLoading ?? false || !snapshot.hasData;
-
-                  if (isLoading) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-
-                  bool hasError = snapshot.data?.hasError ?? false;
-
-                  if (hasError) {
-                    return const Center(
-                      child: Text("Terjadi kesalahan"),
-                    );
-                  }
-
-                  List<BusinessData> businessData =
-                      snapshot.data?.businessData ?? [];
-
-                  if (businessData.isEmpty) {
-                    return const Center(
-                      child: Text("Data tidak ditemukan"),
-                    );
-                  }
-
-                  return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: businessData.length,
-                      itemBuilder: (context, index) {
-                        BusinessData data = businessData[index];
-                        return BusinessCard(businessData: data);
-                      });
-                })
-          ],
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  widget.selectedBusiness.add(null);
+                },
+                child: const Icon(Icons.arrow_back),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: MySearchBar(
+                    isRounded: true,
+                    onChanged: (value) {
+                      _searchStream.add(value);
+                    },
+                    label: "Cari EO atau Vendor yang kamu inginkan!"),
+              ),
+            ],
+          ),
         ),
-      ),
+        const SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: StreamBuilder<ServiceState>(
+              stream: _serviceBloc.stream,
+              builder: (context, snapshot) {
+                bool isLoading =
+                    snapshot.data?.isLoading ?? false || !snapshot.hasData;
+
+                bool hasError = snapshot.data?.hasError ?? false;
+
+                if (hasError) {
+                  return const Center(
+                    child: Text("Terjadi kesalahan"),
+                  );
+                }
+
+                List<BusinessData> businessData = snapshot.data?.businessData ??
+                    List.generate(5, (_) => BusinessData.dummy());
+
+                if (businessData.isEmpty) {
+                  return const Center(
+                    child: Text("Data tidak ditemukan"),
+                  );
+                }
+
+                return RefreshIndicator(
+                  onRefresh: () async {
+                    _serviceBloc.getServices(categoryId: categoryId!);
+                  },
+                  child: Skeletonizer(
+                    enabled: isLoading,
+                    child: ListView.separated(
+                        shrinkWrap: true,
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 100),
+                        itemCount: businessData.length,
+                        separatorBuilder: (context, index) => const SizedBox(
+                              height: 20,
+                            ),
+                        itemBuilder: (context, index) {
+                          BusinessData data = businessData[index];
+                          return BusinessCard(businessData: data);
+                        }),
+                  ),
+                );
+              }),
+        )
+      ],
     );
   }
 }
