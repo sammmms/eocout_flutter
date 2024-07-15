5b0f4b699fc9295f0efa0c6f5da51e77758d4541
sammmms <samuelonasis23@gmail.com>
2024-07-15 13:29:02 +0700

    feat: booking data

---
+++ lib/models/booking_data.dart
@@ -0,0 +1,48 @@
+import 'package:eocout_flutter/models/business_data.dart';
+import 'package:eocout_flutter/utils/status_util.dart';
+
+class BookingData {
+  DateTime bookingDate;
+  String id;
+  Status paymentStatus;
+  BusinessData businessData;
+  Status status;
+
+  BookingData({
+    required this.bookingDate,
+    required this.id,
+    required this.paymentStatus,
+    required this.businessData,
+    required this.status,
+  });
+
+  factory BookingData.fromJson(Map<String, dynamic> json) {
+    return BookingData(
+      bookingDate: DateTime.parse(json['booking_date']).toLocal(),
+      id: json['id'],
+      paymentStatus: StatusUtil.fromText(json['payment_status']),
+      businessData: BusinessData.fromJson(json['service']),
+      status: StatusUtil.fromText(json['status']),
+    );
+  }
+
+  factory BookingData.empty() {
+    return BookingData(
+      bookingDate: DateTime.now(),
+      id: '',
+      paymentStatus: Status.pending,
+      businessData: BusinessData.empty(),
+      status: Status.pending,
+    );
+  }
+
+  factory BookingData.dummy() {
+    return BookingData(
+      bookingDate: DateTime.now(),
+      id: '1',
+      paymentStatus: Status.pending,
+      businessData: BusinessData.dummy(),
+      status: Status.pending,
+    );
+  }
+}
