1d5a6e75d0234bb6b4bdd7d3641a4cafeff35005
sammmms <samuelonasis23@gmail.com>
2024-07-15 13:28:30 +0700

    fix: remove unnecessary EO RegisterData

--- lib/models/register_data.dart
+++ lib/models/register_data.dart
@@ -1,18 +1,15 @@
-import 'package:eocout_flutter/utils/business_type_util.dart';
 import 'package:eocout_flutter/utils/role_type_util.dart';
 
 class RegisterData {
   String username;
   String email;
   String password;
-  String passwordConfirmation;
   UserRole role;
 
   RegisterData(
       {this.username = "",
       this.email = "",
       this.password = "",
-      this.passwordConfirmation = "",
       this.role = UserRole.user});
 
   Map<String, dynamic> toJson() => {
@@ -21,49 +18,9 @@ class RegisterData {
         'username': username,
         'role': UserRoleUtil.textOf(role),
       };
-}
-
-class EOREgisterData extends RegisterData {
-  String? province;
-  String? city;
-  String? bankAccountNumber;
-  String? npwpNumber;
-  String identityNumber;
-  String phoneNumber;
-  String nibNumber;
-  BusinessType? businessType;
-
-  EOREgisterData({
-    this.province,
-    this.city,
-    this.bankAccountNumber,
-    this.identityNumber = "",
-    this.npwpNumber,
-    this.phoneNumber = "",
-    this.nibNumber = "",
-    this.businessType,
-  }) : super(
-            username: "",
-            email: "",
-            password: "",
-            passwordConfirmation: "",
-            role: UserRole.eventOrganizer);
 
   @override
-  Map<String, dynamic> toJson() => {
-        'username': username,
-        'email': email,
-        'password': password,
-        'role': UserRoleUtil.textOf(role),
-        'province': province,
-        'city': city,
-        'accountNumber': bankAccountNumber,
-        'identityNumber': identityNumber,
-        'npwpNumber': npwpNumber,
-        'phoneNumber': phoneNumber,
-        'nibNumber': nibNumber,
-        'businessType': businessType == null
-            ? null
-            : BusinessTypeUtil.textOf(businessType!),
-      };
+  String toString() {
+    return 'RegisterData{username: $username, email: $email, password: $password, role: $role}';
+  }
 }
