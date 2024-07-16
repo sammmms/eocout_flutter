1a3f968da50d5acb23878105df7f40ef4e51639a
sammmms <samuelonasis23@gmail.com>
2024-07-16 10:18:40 +0700

    fix: layout in recommendation carousel EO

--- lib/features/homepage_eo/widget/eo_recommendation_carousel.dart
+++ lib/features/homepage_eo/widget/eo_recommendation_carousel.dart
@@ -1,6 +1,18 @@
 import 'package:carousel_slider_plus/carousel_slider_plus.dart';
 import 'package:flutter/material.dart';
 
+List<String> title = [
+  'Kelas EO Festival',
+  'Kelas EO Publik',
+  'Kelola Pernikahan',
+];
+
+List<String> subtitle = [
+  'Pelajari cara mengelola festival dengan baik',
+  'Pelajari cara mengelola event publik dengan baik',
+  'Pelajari cara mengelola pernikahan dengan baik',
+];
+
 class EoRecommendationCarousel extends StatelessWidget {
   const EoRecommendationCarousel({super.key});
 
@@ -8,9 +20,9 @@ class EoRecommendationCarousel extends StatelessWidget {
   Widget build(BuildContext context) {
     return CarouselSlider(
       items: [
-        _buildCarouselItems('assets/images/festive_culture.png'),
-        _buildCarouselItems('assets/images/public_event.png'),
-        _buildCarouselItems('assets/images/wedding.png'),
+        _buildCarouselItems('assets/images/festive_culture.png', 0),
+        _buildCarouselItems('assets/images/public_event.png', 1),
+        _buildCarouselItems('assets/images/wedding.png', 2),
       ],
       options: CarouselOptions(
           autoPlay: true,
@@ -23,13 +35,46 @@ class EoRecommendationCarousel extends StatelessWidget {
     );
   }
 
-  Widget _buildCarouselItems(String path) {
+  Widget _buildCarouselItems(String path, int idx) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Image.asset(path, fit: BoxFit.cover, width: double.infinity),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Stack(
+            children: [
+              Image.asset(path, fit: BoxFit.cover, width: double.infinity),
+              Container(
+                color: Colors.black.withOpacity(0.4),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      title[idx],
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      subtitle[idx],
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300),
+                      textAlign: TextAlign.end,
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
     );
   }
 }
