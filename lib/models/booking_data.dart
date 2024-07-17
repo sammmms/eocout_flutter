6be7edeba33968f6e685026ddadc7b693ab82693
sammmms <samuelonasis23@gmail.com>
2024-07-17 14:02:11 +0700

    feat: payment status in booking data

--- lib/models/booking_data.dart
+++ lib/models/booking_data.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
 import 'package:eocout_flutter/models/business_data.dart';
+import 'package:eocout_flutter/utils/payment_status_util.dart';
 import 'package:eocout_flutter/utils/status_util.dart';
 
 class BookingData {
   DateTime bookingDate;
   String id;
-  Status paymentStatus;
-  BusinessData businessData;
+  PaymentStatus paymentStatus;
   Status status;
+  BusinessData businessData;
 
   BookingData({
     required this.bookingDate,
@@ -23,7 +24,7 @@ class BookingData {
     return BookingData(
       bookingDate: DateTime.parse(json['booking_date']).toLocal(),
       id: json['id'],
-      paymentStatus: StatusUtil.fromText(json['payment_status']),
+      paymentStatus: PaymentStatusUtil.fromString(json['payment_status']),
       businessData: BusinessData.fromJson(json['service'],
           images: serviceImage, profilePic: profilePic),
       status: StatusUtil.fromText(json['status']),
@@ -34,7 +35,7 @@ class BookingData {
     return BookingData(
       bookingDate: DateTime.now(),
       id: '',
-      paymentStatus: Status.pending,
+      paymentStatus: PaymentStatus.pending,
       businessData: BusinessData.empty(),
       status: Status.pending,
     );
@@ -44,7 +45,7 @@ class BookingData {
     return BookingData(
       bookingDate: DateTime.now(),
       id: '1',
-      paymentStatus: Status.pending,
+      paymentStatus: PaymentStatus.pending,
       businessData: BusinessData.dummy(),
       status: Status.pending,
     );
