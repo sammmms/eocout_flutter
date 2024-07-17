983c0c06614a266ecd99b8c071c4513bb0d9eee0
sammmms <samuelonasis23@gmail.com>
2024-07-17 13:30:37 +0700

    docs: print error (socket exception and websocketchannelexcpetion)

--- lib/utils/print_error.dart
+++ lib/utils/print_error.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:eocout_flutter/utils/app_error.dart';
 import 'package:flutter/foundation.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 void printError(Object err, {String? method}) {
   if (kDebugMode) {
@@ -27,6 +28,18 @@ void printError(Object err, {String? method}) {
         print("response is not a map");
         print(err.response);
       }
+      return;
+    }
+    if (err is SocketException) {
+      print('Tidak ada internet koneksi');
+      return;
+    }
+    if (err is WebSocketChannelException) {
+      if (err is WebSocketException) {
+        print(err.message);
+        return;
+      }
+      print(err.message);
       return;
     }
     print(err);
