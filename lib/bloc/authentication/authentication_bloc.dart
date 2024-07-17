8c8b621bf5faa3b41b240cf6c1700a43f63f6e7a
sammmms <samuelonasis23@gmail.com>
2024-07-17 13:25:16 +0700

    fix: send fcm token

--- lib/bloc/authentication/authentication_bloc.dart
+++ lib/bloc/authentication/authentication_bloc.dart
@@ -47,11 +47,11 @@ class AuthBloc {
   Future checkLogin() async {
     try {
       var token = await Store.getToken();
-      if (token != null && token.isNotEmpty) {
-        refreshProfile();
+      if (token.isNotEmpty) {
+        await refreshProfile();
       }
     } catch (err) {
-      printError(err);
+      printError(err, method: "checkLogin");
       return _updateError(err);
     }
   }
@@ -60,9 +60,24 @@ class AuthBloc {
     try {
       _updateStream(AuthState.authenticating());
 
-      var responseToken = await dio.post('/auth/login', data: data.toJson());
+      Map<String, dynamic> dataMap = data.toJson();
+
+      String fcmToken = await Store.getFCMToken();
+
+      dataMap['fcm_token'] = fcmToken;
+
+      if (kDebugMode) {
+        print(dataMap);
+      }
+
+      var responseToken = await dio.post('/auth/login', data: dataMap);
       var responseData = responseToken.data['data'];
       String token = responseData['token'];
+
+      if (kDebugMode) {
+        print("Token: $token");
+      }
+
       await Store.saveToken(token);
 
       dio.interceptors.add(TokenInterceptor());
@@ -84,17 +99,16 @@ class AuthBloc {
             profilePicture: await ImageBloc().loadImage(userData.pictureId));
       }
 
-      await Store.saveUser(userData);
       _updateStream(AuthState(user: userData));
     } catch (err) {
-      printError(err);
+      printError(err, method: "login");
       return _updateError(err);
     }
     return null;
   }
 
   void logout() {
-    Store.clearStore();
+    Store.clearAuthenticationStore();
     _updateStream(AuthState());
   }
 
@@ -113,7 +127,7 @@ class AuthBloc {
       _updateStream(AuthState.resetState());
       return null;
     } catch (err) {
-      printError(err);
+      printError(err, method: "register");
       return _updateError(err);
     }
   }
@@ -138,10 +152,9 @@ class AuthBloc {
             profilePicture: await ImageBloc().loadImage(userData.pictureId));
       }
 
-      await Store.saveUser(userData);
       _updateStream(AuthState(user: userData));
     } catch (err) {
-      printError(err);
+      printError(err, method: "refreshProfile");
       return _updateError(err);
     }
   }
@@ -151,17 +164,20 @@ class AuthBloc {
       UserData? currentUser = state?.user;
 
       if (currentUser == null) {
-        return AppError('User not found', 401);
+        return AppError('Terjadi kesalahan autentikasi', 401);
       }
       var response = await dio.post('/auth/verify-email',
           data: {'email': currentUser.email, 'verification_code': otpCode});
       if (response.statusCode == 200) {
+        if (kDebugMode) {
+          print("Verify Email Success");
+        }
         return null;
       }
-      return AppError('Failed to verify email');
+      return AppError('Gagal verifikasi email');
     } catch (err) {
-      printError(err);
-      return _updateError(err);
+      printError(err, method: "verifyEmail");
+      return AppError.fromErr(err);
     }
   }
 
@@ -170,17 +186,20 @@ class AuthBloc {
       UserData? currentUser = state?.user;
 
       if (currentUser == null) {
-        return AppError('User not found', 401);
+        return AppError('Terjadi kesalahan autentikasi', 401);
       }
       var response = await dio
           .post('/auth/resend-code', data: {'email': currentUser.email});
       if (response.statusCode == 200) {
+        if (kDebugMode) {
+          print("Resend OTP Code Success");
+        }
         return null;
       }
-      return AppError('Failed to resend OTP code');
+      return AppError('Gagal mengirim ulang kode OTP');
     } catch (err) {
-      printError(err);
-      return _updateError(err);
+      printError(err, method: "resendOTPCode");
+      return AppError.fromErr(err);
     }
   }
 }
