d4fe301c1cd93c640efb8af57f8300bb406812b1
Sam! <156488637+sammmms@users.noreply.github.com>
2024-07-19 04:00:40 +0700

    fix: layouting in notification card

--- lib/features/notification/widget/notification_card.dart
+++ lib/features/notification/widget/notification_card.dart
@@ -1,4 +1,7 @@
+import 'package:eocout_flutter/components/my_avatar_loader.dart';
 import 'package:eocout_flutter/models/notification_data.dart';
+import 'package:eocout_flutter/utils/notification_type_util.dart';
+import 'package:eocout_flutter/utils/theme_data.dart';
 import 'package:flutter/material.dart';
 
 class NotificationCard extends StatelessWidget {
@@ -24,10 +27,28 @@ class NotificationCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            data.content,
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
+          if (data.user != null)
+            Row(
+              children: [
+                MyAvatarLoader(user: data.user!),
+                const SizedBox(width: 10),
+                Text(
+                  data.user!.fullname,
+                  style: textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              Icon(
+                NotificationTypeUtil.iconOf(data.type),
+                color: colorScheme.secondaryContainer,
+              ),
+              const SizedBox(width: 10),
+              Expanded(child: Text(data.content, style: textTheme.bodyMedium)),
+            ],
+          )
         ],
       ),
     );
