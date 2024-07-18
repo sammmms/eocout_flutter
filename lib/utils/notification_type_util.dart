2b7fc216e5bb9d197a2e3834c6f267107f7d1c74
Sam! <156488637+sammmms@users.noreply.github.com>
2024-07-19 04:02:40 +0700

    feat: icon getter in notif util

--- lib/utils/notification_type_util.dart
+++ lib/utils/notification_type_util.dart
@@ -1,4 +1,5 @@
 import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
 
 enum NotificationType { userBooking, chat, payment }
 
@@ -16,5 +17,16 @@ class NotificationTypeUtil {
 
   static String textOf(NotificationType value) {
     return notificationTypeMap[value]!;
+  }
+
+  static IconData iconOf(NotificationType value) {
+    switch (value) {
+      case NotificationType.userBooking:
+        return Icons.notifications;
+      case NotificationType.chat:
+        return Icons.chat;
+      case NotificationType.payment:
+        return Icons.payment;
+    }
   }
 }
