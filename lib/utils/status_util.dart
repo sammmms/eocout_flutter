3e2b82a293e20828dca1d02bee45e00cf90983d8
sammmms <samuelonasis23@gmail.com>
2024-07-19 15:38:50 +0700

    chore: ordering status

--- lib/utils/status_util.dart
+++ lib/utils/status_util.dart
@@ -1,4 +1,4 @@
-enum Status { pending, confirmed, cancelled, completed, all }
+enum Status { pending, confirmed, completed, cancelled, all }
 
 class StatusUtil {
   static const Map<Status, String> statusMap = {
